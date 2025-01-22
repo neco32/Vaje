dc59f427dfbdc6e14a75f8dae13c8a50543d4157
študent <student@c.scng.si>
2025-01-22 10:37:52 +0100

    Add initial implementation for Vaja5 project with basic temperature analysis

---
+++ Vaja5/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Napiši temperaturo vode v stopinjah celzija: ");
+        double temp = double.Parse(Console.ReadLine());
+        if (temp <= 10.0){
+            Console.WriteLine("Voda je mrzla");
+        } else if (temp <= 20.0){
+            Console.WriteLine("Voda je hladna");
+        }else if (temp <= 30.0){
+            Console.WriteLine("Voda je topla");
+        }else if (temp <= 50.0){
+            Console.WriteLine("Voda je vroča");
+        }
+        
+    }
+}

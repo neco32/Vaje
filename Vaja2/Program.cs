6323ae480651fb68360a011674f1af7a719c2cd3
študent <student@c.scng.si>
2025-01-22 09:59:24 +0100

    To je commit 2. naloga

--- Vaja2/Program.cs
+++ Vaja2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Vaja2");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /**/
+        Console.WriteLine("Vnesi leto");
+        int leto = int.Parse(Console.ReadLine());
+        if(leto%4==0 && (leto%100!=0 || leto%400==0))
+        {
+            Console.WriteLine("Leto je prestopno - praznovala bo 29. februarja");
+        }
+        else
+        {
+            Console.WriteLine("Leto ni prestopno - praznovala bo 1. marca");
+        }
+    }
+}

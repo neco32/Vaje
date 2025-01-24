6d6a4afcbd5bd83179af6417577fc54601a1a117
vid5 <ursicvid2@gmail.com>
2025-01-24 08:14:21 +0100

    Vaja 1 zanke

---
+++ Zanke/Vaja1/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Vpiši število členov zaporedja: ");
+        int stclenov = int.Parse(Console.ReadLine());
+        int vsota = 0;
+        int n = 1;
+        while (n <= stclenov)
+        {
+            int clen = (n+1)*(n-1);
+            Console.WriteLine(n+" člen zaporedja: "+ clen);
+            vsota = vsota + n;
+            n++;
+        }
+    }
+}

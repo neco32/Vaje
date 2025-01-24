219e7b8eab2cde1aa19eb27d6855cb2d9a9aed55
vid5 <ursicvid2@gmail.com>
2025-01-24 08:34:57 +0100

    Vaja 2

---
+++ Zanke/Vaja2(while)/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Vnesi število predmetov: ");
+        int stPredmetov = int.Parse(Console.ReadLine());
+        int stevec = 0;
+        int vsota = 0;
+        int najnižja = 10;
+        int najvišja = 0;
+        while (stevec <= stPredmetov)
+        {
+            Console.WriteLine("Vnesi "+stevec+" oceno: ");
+            int ocena = int.Parse(Console.ReadLine());
+            if (ocena < 1 || ocena > 10)
+            {
+                Console.WriteLine("Ocena mora biti med 1 in 10");
+                continue;
+            }
+            vsota += ocena;
+            stevec++;
+            najnižja = Math.Min(najnižja, ocena);
+            najvišja = Math.Max(najvišja, ocena);
+        }
+        Console.WriteLine("Najnižja ocena: "+najnižja);
+        Console.WriteLine("Najvišja ocena: "+najvišja);
+        Console.WriteLine("Povprečna ocena: "+((double)vsota/stPredmetov));
+    }
+}

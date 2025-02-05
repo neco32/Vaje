1967e155ff02f5387760e0a6b03fd00d571ee914
študent <student@c.scng.si>
2025-02-05 09:48:40 +0100

    Add initial project files and configuration for Vaja3 2d tabela

---
+++ Tabele/Vaja3 2d tabela/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] tabela = new int[10, 10];
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                tabela[i, j] = rnd.Next(101);
+                //Console.Write(tabela[i, j] + "\t");
+                Console.Write("{0, 5}", tabela[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}

0e2d3d314169625ea6217797b5577e26505bc3b0
študent <student@c.scng.si>
2025-02-05 10:50:43 +0100

    Fix game logic in Vaja 5 krizci krozci: update empty cell checks and improve user prompts

--- Tabele/Vaja 5 krizci krozci/Program.cs
+++ Tabele/Vaja 5 krizci krozci/Program.cs
@@ -7,55 +7,53 @@
         {
             for (int j = 0; j < 3; j++)
             {
-                polje[i, j] = null;
+                polje[i, j] = "1";
             }
         }
         while (true)
         {
+            IzpisiPolje(polje);
             Console.WriteLine("Vnesi vrstico in stolpec (1-3) za igralca X:");
             int vrstica = int.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine("Vnesi vrstico in stolpec (1-3) za igralca X:");
             int stolpec = int.Parse(Console.ReadLine()) - 1;
-            if (polje[vrstica, stolpec] != null)
+            if(polje[vrstica, stolpec] != "1")
             {
-                Console.WriteLine("Mesto je že zasedeno, poskusite znova.");
+                Console.WriteLine("Polje je že zasedeno!");
                 continue;
             }
             polje[vrstica, stolpec] = "X";
-            if (!PreveriZmago(polje))
+            if (PreveriZmago(polje))
             {
-                IzpisiPolje(polje);
                 Console.WriteLine("Zmagal je igralec X!");
                 break;
             }
             if (!PreveriPrazno(polje))
             {
-                IzpisiPolje(polje);
                 Console.WriteLine("Igra je neodločena!");
                 break;
-            if (polje[vrstica, stolpec] != null)
-            {
-                Console.WriteLine("Mesto je že zasedeno, poskusite znova.");
-                continue;
             }
-            polje[vrstica, stolpec] = "O";
             IzpisiPolje(polje);
             Console.WriteLine("Vnesi vrstico in stolpec (1-3) za igralca O:");
             vrstica = int.Parse(Console.ReadLine()) - 1;
             stolpec = int.Parse(Console.ReadLine()) - 1;
+            if(polje[vrstica, stolpec] != "1")
+            {
+                Console.WriteLine("Polje je že zasedeno!");
+                continue;
+            }
             polje[vrstica, stolpec] = "O";
             if (PreveriZmago(polje))
             {
-                IzpisiPolje(polje);
                 Console.WriteLine("Zmagal je igralec O!");
                 break;
             }
             if (!PreveriPrazno(polje))
             {
-                IzpisiPolje(polje);
                 Console.WriteLine("Igra je neodločena!");
                 break;
             }
-            IzpisiPolje(polje);
+
         }
 
     }
@@ -63,20 +61,20 @@
     {
         for (int i = 0; i < 3; i++)
         {
-            if (polje[i, 0] == polje[i, 1] && polje[i, 1] == polje[i, 2])
+            if (polje[i, 0] != "1" && polje[i, 0] == polje[i, 1] && polje[i, 1] == polje[i, 2])
             {
                 return true;
             }
-            if (polje[0, i] == polje[1, i] && polje[1, i] == polje[2, i])
+            if (polje[0, i] != "1" && polje[0, i] == polje[1, i] && polje[1, i] == polje[2, i])
             {
                 return true;
             }
         }
-        if (polje[0, 0] == polje[1, 1] && polje[1, 1] == polje[2, 2])
+        if (polje[0, 0] != "1" && polje[0, 0] == polje[1, 1] && polje[1, 1] == polje[2, 2])
         {
             return true;
         }
-        if (polje[0, 2] == polje[1, 1] && polje[1, 1] == polje[2, 0])
+        if (polje[0, 2] != "1" && polje[0, 2] == polje[1, 1] && polje[1, 1] == polje[2, 0])
         {
             return true;
         }
@@ -88,7 +86,7 @@
         {
             for (int j = 0; j < 3; j++)
             {
-                if (polje[i, j] == null)
+                if (polje[i, j] == "1")
                 {
                     return true;
                 }
